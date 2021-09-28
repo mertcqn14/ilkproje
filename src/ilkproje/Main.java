b4257d0781e2613ac8516ccc5c5bdd9db8816615
mertcqn14 <mertcanciftcioglu704@gmail.com>
2021-09-28 14:02:17 +0300

    28.09.2021

---
+++ src/ilkproje/Main.java
@@ -0,0 +1,18 @@
+package ilkproje;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Merhaba Java");
+       System.out.print("Mertcan") ;
+       System.out.print(" ");
+       System.out.print("Çiftçioðlu");
+       System.out.print(" ");
+       System.out.print("15");
+       System.out.print(" ");
+       System.out.print("Bolu");
+       
+      }
+
+}
